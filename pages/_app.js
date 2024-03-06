e5e21efb8d9bc3b049b8630ecd266003367dd024
Nadine Meyer Ouro <nadine.meyer.ouro@gmail.com>
2024-03-06 18:03:53 +0000

    adding head title and temporary favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,31 @@
+import Head from "next/head";
 import "../styles/reset.css";
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/images/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/images/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/images/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/images/site.webmanifest" />
+        <title>devchallenges</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
